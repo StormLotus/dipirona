9e749bf20dece024af3d527a04dd673a75c8383a
fvendrameto <fernandovendrameto@gmail.com>
2017-10-28 14:04:54 -0200

    sql tables created

--- csv2table.py
+++ csv2table.py
@@ -4,19 +4,15 @@ import sqlite3
 conn = sqlite3.connect('salarios')
 
 c = conn.cursor()
-c.execute('create table salarios (id int primary key, nome text,remBru real,ind real,red real,desc real,remLiq real);')
+c.execute('create table ago17 (id int primary key, nome text,remLiq real);')
 
-with open('tabela_salarios.csv') as csvfile:
+with open('folhasPagamento/2017-08.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
 		values = str(row['id']) + ','
 		values += "'" + row['nome'] + "'" + ','
-		values += row['remBru'] + ','
-		values += row['ind'] + ','
-		values += row['red'] + ','
-		values += row['desc'] + ','
-		values += row['remLiq']
-		c.execute('insert into salarios values (' + values + ');')
+		values += row['remLiq'] + ''
+		c.execute('insert into ago17 values (' + values + ');')
 
 conn.commit()
 conn.close()
