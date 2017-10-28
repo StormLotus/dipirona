86b38362df569cd31daf41cdd0518a73c1f8cd41
fvendrameto <fernandovendrameto@gmail.com>
2017-10-28 06:13:23 -0200

    id as primary index added

--- csv2table.py
+++ csv2table.py
@@ -2,17 +2,21 @@ import csv
 import sqlite3
 
 conn = sqlite3.connect('salarios')
+
 c = conn.cursor()
-c.execute('create table salarios (nome text,remBru real,ind real,red real,desc real,remLiq real);')
+c.execute('create table salarios (id int primary key, nome text,remBru real,ind real,red real,desc real,remLiq real);')
+
 with open('tabela_salarios.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
-		values = "'" + row['nome'] + "'" + ','
+		values = str(row['id']) + ','
+		values += "'" + row['nome'] + "'" + ','
 		values += row['remBru'] + ','
 		values += row['ind'] + ','
 		values += row['red'] + ','
 		values += row['desc'] + ','
 		values += row['remLiq']
+		#print(values)
 		c.execute('insert into salarios values (' + values + ');')
 
 conn.commit()
